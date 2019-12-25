2519b030c9b322dea2e0c071bce346d7a372e9d9
Md-Rohed <mdrohed85@gmail.com>
2019-12-25 22:56:52 +0600

    add files

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,8 @@ const Hero = () => (
     <main className="cover-page" id="hero">
       <section className="wrapped-page">
         <div className="item-center">
-          <h1>Manila Tourism</h1>
-          <h3>Tours | Travel | Guide</h3>
+          <h1>Explore Bd</h1>
+          <h3>Tours | Travel | </h3>
           <Button outline color="warning" href="#mainCarousel">
             Explore More
           </Button>{" "}
